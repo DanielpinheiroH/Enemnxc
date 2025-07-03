c82c2f7edf8eab3c0ab4102c07063a8d5174175e
Daniel Henrique Pinheiro Lopes de Souza <Daniel.Henrique@METROPOLES.LOCAL>
2025-07-03 15:03:48 -0300

    teste

---
+++ enemnxc/src/types/recharts-fix.d.ts
@@ -0,0 +1,11 @@
+// src/types/recharts-fix.d.ts
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare module "recharts" {
+  export const BarChart: any;
+  export const Bar: any;
+  export const XAxis: any;
+  export const YAxis: any;
+  export const Tooltip: any;
+  export const ResponsiveContainer: any;
+}
